f13a00548e897d25439c3f84fc39d5a9fd5d970e
chhhhhyoo <chanhyuny0515@gmail.com>
2024-02-07 22:50:16 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,87 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello Flutter!"),
-        ),
-        body: const Center(
-          child: Text("This is Home"),
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 70,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hey, BB',
+                        style: TextStyle(
+                            color: Color(0xFFBFE3B4),
+                            fontSize: 24,
+                            fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: const Color(0xFFBFE3B4).withOpacity(0.7),
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 55,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.white.withOpacity(0.5),
+                ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Text(
+                "\$5 194 542",
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                          color: const Color(0xFFF2B33A),
+                          borderRadius: BorderRadius.circular(45)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 38,
+                        ),
+                        child: Text(
+                          "Transfer",
+                          style: TextStyle(
+                              fontSize: 21,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black.withOpacity(0.8)),
+                        ),
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
